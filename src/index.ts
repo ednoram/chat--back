d0db858ffb555eaa3cd48aacac3e77d3c3585fb0
ednoram <e.noramirian@gmail.com>
2021-10-03 17:35:48 +0400

    main

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,48 @@
 import express, { json, urlencoded } from "express";
-import cors from "cors";
 import "dotenv/config";
+import cors from "cors";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 import { PORT } from "@config";
-import { connectDB } from "@utils";
+// import { connectDB } from "@utils";
 
-connectDB();
+// connectDB();
 
 const app = express();
+const httpServer = createServer(app);
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cors({ origin: true, credentials: true }));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const io = new Server(httpServer, { cors: { origin: true } });
+
+io.on("connection", (socket) => {
+  socket.on("message", (message, room) => {
+    const processedRoom = room && room.trim();
+    const processedMessage = message && message.trim();
+
+    if (processedRoom && processedMessage) {
+      io.to(processedRoom).emit("message", processedMessage);
+    }
+  });
+
+  socket.on("room", (room) => {
+    const processedRoom = room && room.trim();
+
+    if (processedRoom) {
+      const socketRooms = Array.from(socket.rooms);
+
+      socketRooms.map((room) => {
+        socket.leave(room);
+      });
+
+      socket.join(processedRoom);
+    }
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
