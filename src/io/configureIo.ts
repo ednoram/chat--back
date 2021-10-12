643dc3b5e2ed76a9e4457799d1fe81ef726bbb5c
ednoram <e.noramirian@gmail.com>
2021-10-12 21:47:21 +0400

    main

--- src/io/configureIo.ts
+++ src/io/configureIo.ts
@@ -3,16 +3,19 @@ import { Server } from "node_modules/socket.io/dist";
 const configureIo = (io: Server): void => {
   io.on("connection", (socket) => {
     socket.on("message", (message, room) => {
-      const processedRoom = room && room.trim();
-      const processedMessage = message && message.trim();
+      const processedRoom = room?.trim();
+      const processedMessage = {
+        ...message,
+        text: message?.text?.trim(),
+      };
 
-      if (processedRoom && processedMessage) {
+      if (processedRoom && processedMessage.text && processedMessage.user) {
         io.to(processedRoom).emit("message", processedMessage);
       }
     });
 
     socket.on("room", (room) => {
-      const processedRoom = room && room.trim();
+      const processedRoom = room?.trim();
 
       if (processedRoom) {
         const socketRooms = Array.from(socket.rooms);
